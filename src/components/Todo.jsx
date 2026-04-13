6a277f420a323a64c7d7459ff2339f4592184b47
Fathima Hanna <fathimahannap123@gmail.com>
2026-04-13 18:08:32 +0530

    Refactor Todo component layout; add input box

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -13,6 +13,16 @@ const Todo = () => {
         <h1 className='text-3xl font-semibold text-emerald-100'>To-Do List</h1>
     </div>
 
+
+{/*----------input box----------------*/}
+
+
+    <div className='flex items-center my-7 bg-emerald-100 rounded-full'>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add Your Task'/>
+        <button className='border-none rounded-full bg-emerald-700 hover:bg-emerald-950 w-15 h-9 mr-3 text-emerald-100 fond-medium cursor-pointer'>Add+</button>
+    </div>
+
+
     </div>
   )
 }
